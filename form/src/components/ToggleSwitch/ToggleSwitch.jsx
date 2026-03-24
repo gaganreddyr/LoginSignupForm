408d5fc5b1dd006e2dc17ca248fb73527da890c5
gaganreddyr <gagan.ravichandrareddy@gmail.com>
2026-03-24 17:20:11 +0530

    created new reusable toggle switch component

---
+++ form/src/components/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./ToggleSwitch.css";
+
+const ToggleSwitch = ({ checked, onChange, label }) => {
+  return (
+    <div className="toggle-container">
+      {label && <span className="toggle-label">{label}</span>}
+      
+      <label className="switch">
+        <input
+          type="checkbox"
+          checked={checked}
+          onChange={onChange}
+        />
+        <span className="slider">
+          <span className="slider-text">
+            {checked ? "Dark" : "Light"}
+          </span>
+        </span>
+      </label>
+    </div>
+  );
+};
+
+export default ToggleSwitch;
